578c55341f83b5ccb6a5b2b0400f65dc807c0664
OLUWABUKUNMI  OJO <88961905+Chrysz-Ojo@users.noreply.github.com>
2023-09-17 04:42:56 +0100

    Update CustomerController.java

--- src/main/java/com/example/render/controller/CustomerController.java
+++ src/main/java/com/example/render/controller/CustomerController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.List;;
 
 @RestController
 @RequestMapping("api/v1/customers")
